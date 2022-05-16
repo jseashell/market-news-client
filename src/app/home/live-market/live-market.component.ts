83b9472d61560b18e77c110395b9992ca642bfc8
John Schellinger <schellingerj1@gmail.com>
2022-05-16 00:16:37 -0400

    Add finnhub-api connector

--- src/app/home/live-market/live-market.component.ts
+++ src/app/home/live-market/live-market.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { webSocket } from 'rxjs/webSocket';
 
 @Component({
   selector: 'app-live-market',
   templateUrl: './live-market.component.html',
-  styleUrls: ['./live-market.component.scss']
+  styleUrls: ['./live-market.component.scss'],
 })
 export class LiveMarketComponent implements OnInit {
+  ws: string = 'No response';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    const subject = webSocket('wss://1jl4s4n5z4.execute-api.us-east-1.amazonaws.com/dev/connectHandler');
+    subject.subscribe((msg) => {
+      this.ws = JSON.stringify(msg);
+      console.log('message received:\n' + this.ws);
+    });
   }
-
 }
