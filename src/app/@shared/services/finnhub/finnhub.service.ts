8394b557eded91246813d3c38bffbe8f273128ee
John Schellinger <schellingerj1@gmail.com>
2022-06-09 15:27:58 -0400

    Create a service for Finnhub REST endpoints

---
+++ src/app/@shared/services/finnhub/finnhub.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FinnhubService {
+  constructor(private http: HttpClient) {}
+
+  getSymbols() {
+    return this.http.get(`https://finnhub.io/api/v1/stock/symbol?exchange=US&token=${environment.finnhub.token}`);
+  }
+}
